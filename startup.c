396ed115dd0b3c9518ed8631cc94b3380e63ea7a
Welington Panzarini <121058195+WelingtonPanzarini@users.noreply.github.com>
2023-10-27 15:18:57 -0300

    tela cadastro
    inicio  tela de cadastro

--- startup.c
+++ startup.c
@@ -6,29 +6,36 @@ int opcao = 0;
 
 void TelaLogin(){
 
-    char login[15] = "admin";
-    char login1[15];
-    char senha[15] = "admin";
-    char senha1[15];
+        char login[15] = "admin";
+        char login1[15];
+        char senha[15] = "admin";
+        char senha1[15];
 
-    printf("digite seu Login");
-    scanf("%s", login1);
+        printf("digite seu Login\n ");
+        scanf("%s", login1);
 
-    printf("digite sua Senha");
-    scanf("%s", senha1);
+        printf("digite sua Senha\n ");
+        scanf("%s", senha1);
 
-    if(strcmp(login, login1) == 0 && strcmp(senha, senha1) == 0){
-        printf("\nLogado\n");
-    }else{
-        printf("\nDados inválidos\n");
+        if(strcmp(login, login1) == 0 && strcmp(senha, senha1) == 0){
+            printf("\nLogado\n");
+        }else{
+            printf("\nDados inválidos\n");
+        }
     }
-    MostrarCadastro(){
+
+    void MostrarMenu(){
+
         printf("Seja bem vindo a StartUP");
-        printf("1) - Cadastro de Clientes\n");
-        printf("2) - Relatorios\n");
+        printf("1 - Cadastro de Clientes\n");
+        printf("2 - Relatorios\n");
+        printf("3 - Sair\n");
         scanf("%d", &opcao);
         getchar();
     }
+    int main(){
+        TelaLogin();
+    }
+    
 
 
-}
