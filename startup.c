849e35d9253ee6da58dd8bb917c8c280b6b90e21
Welington Panzarini <121058195+WelingtonPanzarini@users.noreply.github.com>
2023-10-27 17:04:39 -0300

    cadastro feito
    tela de cadastro apenas com o nome, tel, end.

--- startup.c
+++ startup.c
@@ -26,15 +26,65 @@ void TelaLogin(){
 
     void MostrarMenu(){
 
-        printf("Seja bem vindo a StartUP");
+        printf("Seja bem vindo a StartUP\n");
         printf("1 - Cadastro de Clientes\n");
         printf("2 - Relatorios\n");
-        printf("3 - Sair\n");
+        printf("3 - Sair\n\n");
         scanf("%d", &opcao);
         getchar();
     }
+
+    struct Cliente
+    {
+        /* data */
+        char nome[50];
+        char endereco[100];
+        char telefone[16];
+    };
+    
+
+    void CadastroCliente()
+    {   
+        struct Cliente cliente;
+        {
+            /* data */
+            printf("Escreva o nome do Cliente: ");
+            scanf("%s", cliente.nome);
+
+            printf("Escreva o endereço do Cliente: ");
+            scanf("%s", cliente.endereco);
+
+            printf("Escreva o telefone do cliente: ");
+            scanf("%s", cliente.telefone);
+
+            /*FILE *arquivo = fopen("clientes.txt", "a");
+              fprintf(arquivo,"%s;%s;%s\n", cliente.nome, cliente.endereco, cliente.telefone );
+              fclose(arquivo);*/
+
+            FILE *arquivo = fopen("clientes.txt", "r");
+            char nome[50], endereco[100], telefone[16];
+
+            while (fscanf(arquivo, "%[^;];%[^;];%s\n", nome, endereco, telefone) == 3)
+            {
+                /* code */
+                printf("Nome: %s\n", nome);
+                printf("Endereco: %s\n", endereco);
+                printf("Telefone: %s\n", telefone);
+            }
+            fclose(arquivo);
+            
+        }
+        
+
+    }
     int main(){
+
+        setlocale(LC_ALL,"Portuguese");
         TelaLogin();
+        MostrarMenu();
+        CadastroCliente();
+
+        return 0;
     }
     
 
